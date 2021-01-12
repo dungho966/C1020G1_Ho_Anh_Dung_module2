12eae04129e29a6eb89fe1aafc0b0571e109f584
dungho966 <dunghoanh1996@gmail.com>
2021-01-12 20:48:47 +0700

    update

--- module3/case_study_module3/caseStudy.sql
+++ module3/case_study_module3/caseStudy.sql
@@ -1,96 +1,119 @@
-drop database if exists case_study;
 create database case_study;
+
 use case_study;
-create table employee_position(
-	id_position int not null primary key,
-	position_name varchar(50)
+
+CREATE TABLE position_employee (
+    position_id INT PRIMARY KEY,
+    position_name INT
 );
- create table employee_level(
-	id_level int not null primary key,
-	level_name varchar(50)
- );
-create table employee_department(
-	id_department int not null primary key,
-	department_name varchar(50)
+
+CREATE TABLE level_employee (
+    level_id INT PRIMARY KEY,
+    level_name VARCHAR(45)
 );
-create table employee (
-	id_employee INT not null primary key,
-	empolyee_name varchar(50),
-	id_position INT,
-	id_level INT,
-	id_department INT,
-	date_of_birth date,
-	id_of_employee varchar(50),
-	salary varchar(50),
-	phone_number varchar(50),
-	email varchar(50),
-	address varchar(50),
-	foreign key (id_position) references employee_position (id_position),
-	foreign key (id_level) references employee_level (id_level),
-	foreign key (id_department) references employee_department (id_department)
+
+CREATE TABLE department (
+    department_id INT PRIMARY KEY,
+    department_name VARCHAR(45)
 );
-create table type_customer (
-	id_customer_type int not null primary key,
-	type_name varchar(50)
+
+CREATE TABLE employee (
+    employee_id INT PRIMARY KEY,
+    employee_fullname VARCHAR(45),
+    position_id INT,
+    level_id INT,
+    department_id INT,
+    date_of_birth DATE,
+    identity_number VARCHAR(45),
+    salary INT,
+    phone VARCHAR(45),
+    email VARCHAR(45),
+    address VARCHAR(45),
+    FOREIGN KEY (position_id)
+        REFERENCES position_employee (position_id),
+    FOREIGN KEY (level_id)
+        REFERENCES level_employee (level_id),
+    FOREIGN KEY (department_id)
+        REFERENCES department (department_id)
 );
-create table customer (
-	id_customer int not null primary key,
-	id_customer_type int,
-	customer_name varchar (50),
-	date_of_birth date,
-	id_of_customer varchar(50),
-	phone_number varchar(50),
-	email varchar(50),
-	address varchar(50),
-	foreign key (id_customer_type) references type_customer (id_customer_type)
+
+CREATE TABLE service_included (
+    service_included_id INT PRIMARY KEY,
+    service_included_name VARCHAR(45),
+    price INT,
+    unit INT,
+    available_status VARCHAR(45)
 );
-create table rent_type (
-	id_rent_type int not null primary key,
-	rent_type_name varchar(50),
-	cost int
+
+CREATE TABLE rent_type (
+    rent_type_id INT PRIMARY KEY,
+    rent_type_name VARCHAR(45),
+    price INT
 );
-create table services_type (
-	id_services_type int not null primary key,
-	services_name varchar(50)
+
+CREATE TABLE service_type (
+    service_type_id INT PRIMARY KEY,
+    service_type_name VARCHAR(45)
 );
-create table services (
-	id_services int not null primary key,
-	services_name varchar(50),
-	area int,
-	floor int,
-	max_person int,
-	cost int,
-	id_rent_type int,
-	id_services_type int,
-	`status` varchar(50),
-	foreign key (id_rent_type) references rent_type (id_rent_type),
-	foreign key (id_services_type) references services_type (id_services_type)
+
+CREATE TABLE service (
+    service_id INT PRIMARY KEY,
+    service_name VARCHAR(45),
+    area DOUBLE,
+    num_of_floor INT,
+    quantity_max INT,
+    rent_cost INT,
+    rent_type_id INT,
+    service_type_id INT,
+    service_status VARCHAR(45),
+    FOREIGN KEY (service_type_id)
+        REFERENCES service_type (service_type_id),
+    FOREIGN KEY (rent_type_id)
+        REFERENCES rent_type (rent_type_id)
 );
-create table contract(
-	id_contract int not null primary key,
-	id_employee int,
-	id_customer int,
-	id_services int,
-	day_start date,
-	day_end date,
-	deposits int,
-	total_cost int,
-	foreign key (id_employee) references employee (id_employee),
-	foreign key (id_customer) references customer (id_customer),
-	foreign key (id_services) references services (id_services)
+
+CREATE TABLE customer_type (
+    customer_type_id INT PRIMARY KEY,
+    customer_type_name VARCHAR(45)
 );
-create table include_services (
-	id_include_services int not null primary key,
-	include_services_name varchar(50),
-	price int,
-	count int,
-	is_in_use varchar(5)
+
+CREATE TABLE customer (
+    customer_id INT PRIMARY KEY,
+    customer_type_id INT,
+    customer_fullname VARCHAR(45),
+    date_of_birth DATE,
+    identity_number VARCHAR(45),
+    phone VARCHAR(45),
+    email VARCHAR(45),
+    address VARCHAR(45),
+    FOREIGN KEY (customer_type_id)
+        REFERENCES customer_type (customer_type_id)
 );
-create table contract_information (
-	id_information_contract int not null primary key,
-	id_contract int,
-	id_include_services int,
-	quantity int,
-	foreign key (id_include_services) references include_services (id_include_services),
-	foreign key (id_contract) references contract (id_contract)
+
+CREATE TABLE contract (
+    contract_id INT PRIMARY KEY,
+    employee_id INT,
+    customer_id INT,
+    service_id INT,
+    contract_start_date DATE,
+    contract_end_date DATE,
+    deposit_money INT,
+    full_money INT,
+    FOREIGN KEY (employee_id)
+        REFERENCES employee (employee_id),
+    FOREIGN KEY (customer_id)
+        REFERENCES customer (customer_id),
+    FOREIGN KEY (service_id)
+        REFERENCES service (service_id)
+);
+
+CREATE TABLE detail_contract (
+    detail_contract_id INT PRIMARY KEY,
+    contract_id INT,
+    service_included_id INT,
+    amount INT,
+    FOREIGN KEY (service_included_id)
+        REFERENCES service_included (service_included_id),
+    FOREIGN KEY (contract_id)
+        REFERENCES contract (contract_id)
 );
